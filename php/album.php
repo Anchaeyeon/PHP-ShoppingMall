08e7d70d22b2f67f8249c28f5cb4dd388717e819
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-02-26 22:30:35 +0900

    album 페이지 추가 & album 페이지로 이동되도록

---
+++ php/album.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POWER</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/album.css">
+</head>
+<body>
+    <a href="../html/home.html">
+        <div class="rogo">
+            <img src="../img/other/rogo.png" alt="">
+        </div>
+    </a>
+
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../html/home.html">POWER</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../php/album.php">Album</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Goods</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Content</a>
+                    </li>
+                </ul>
+                <a href="../html/login.html">
+                    <img src="../img/other/login.png" alt="Special Page" class="navbar-img">
+                </a>
+                <a href="#">
+                    <img src="../img/other/mypage.png" alt="Special Page" class="navbar-img">
+                </a>
+                <a href="#">
+                    <img src="../img/other/cart.png" alt="Special Page" class="navbar-img">
+                </a>
+            </div>
+        </div>
+    </nav>
+
+    <footer>
+        <p class="footer-rogo"><a href="../html/home.html">POWER</a></p>
+        <div class="icon">
+            <p><a href="#">이용약관</a> | <a href="#">FAQ</a> | <a href="#">문의하기</a> | <a href="#">개인정보처리방침</a></p>
+            <a href="https://www.youtube.com/OfficialGDRAGON"><img src="../img/other/youtube.png" alt="youtube"></a>
+            <a href="https://www.instagram.com/xxxibgdrgn"><img src="../img/other/insta.png" alt="instagram"></a>
+        </div>
+    </footer>
+    
+    
+    
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
